ff574b7695165e188a0aff1571b368c019509e69
Rafael Castro <rafa-v-c@hotmail.com>
2021-10-18 19:25:01 -0300

    Subindo o Desafio 18

---
+++ 03 - EstruturaDeRepeticao/18.py
@@ -0,0 +1,24 @@
+"""
+18 - Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor
+e a soma dos valores.
+"""
+
+qtd_numeros = int(input('Digite a quantidade de números a ser digitado: '))
+
+menor = maior = soma = numero = 0
+
+for i in range(1, qtd_numeros+1):
+    numero = float(input(f'Digite o {i}º número: '))
+    if i == 1:
+        menor = numero
+        maior = numero
+    else:
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            manor = numero
+    soma += numero
+
+print(f"""A soma dos valores é: {soma}
+O maior número é: {maior}
+O menor número é: {menor}""")
