b04921e2931d491db9e6dbfe9a32675a5757bb33
Rafael Castro <rafa-v-c@hotmail.com>
2021-10-18 19:17:11 -0300

    Subindo o Desafio 17

---
+++ 03 - EstruturaDeRepeticao/17.py
@@ -0,0 +1,10 @@
+"""
+17 - Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!=5.4.3.2.1=120
+"""
+numero = int(input('Digite um número e descubra o fatorial do mesmo: '))
+fatorial = 1
+for i in range(1,numero+1):
+    fatorial *= i
+
+print(f'O fatorial de {numero} é igual a {fatorial}')
