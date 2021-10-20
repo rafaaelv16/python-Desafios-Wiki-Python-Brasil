e8c2f62a2ce96888d1a3520b2f6d1a69fa1371a9
Rafael Castro <rafa-v-c@hotmail.com>
2021-10-20 11:38:53 -0300

    Subindo o Desafio 22

---
+++ 03 - EstruturaDeRepeticao/22.py
@@ -0,0 +1,19 @@
+"""
+21 - Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
+Um número primo é aquele que é divisível somente por ele mesmo e por 1.
+"""
+
+numero = int(input('Digite o valor e descubra se é primo'))
+
+contador = 0
+primo = 'não é primo'
+
+for i in range(1,numero):
+    if numero % i == 0:
+        contador += 1
+
+if contador <= 2:
+    primo = 'é primo'
+
+print(f'O número {numero} {primo}')
+
