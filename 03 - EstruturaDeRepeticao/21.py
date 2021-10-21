55700d52e85edb54560432bbae5bdc42c2e99538
Rafael Castro <rafa-v-c@hotmail.com>
2021-10-21 11:46:20 -0300

    Atualizando o Desafio 21

--- 03 - EstruturaDeRepeticao/21.py
+++ 03 - EstruturaDeRepeticao/21.py
@@ -8,11 +8,11 @@ numero = int(input('Digite o valor e descubra se é primo'))
 contador = 0
 primo = 'não é primo'
 
-for i in range(1,numero):
+for i in range(2,numero+1):
     if numero % i == 0:
         contador += 1
 
-if contador <= 2:
+if contador == 1:
     primo = 'é primo'
 
 print(f'O número {numero} {primo}')
